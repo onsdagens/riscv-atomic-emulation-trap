495b1b5068d992125fd32768b3780c4aabcee15c
onsdagens <pawdzi-7@student.ltu.se>
2023-04-12 14:52:20 +0200

    fixed rare bug causing misaligned memory access when emulating atomics

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ pub const PLATFORM_REGISTER_LEN: usize = 32; // TODO will be less on r32e, handl
 
 macro_rules! amo {
     ($frame:ident, $rs1:ident, $rs2:ident, $rd:ident, $operation:expr) => {
-        let tmp = $frame[$rs1];
+        let tmp = $frame[$rs1 as usize];
         let a = *(tmp as *const _);
-        let b = $frame[$rs2];
-        $frame[$rd] = a;
+        let b = $frame[$rs2 as usize];
+        $frame[$rd as usize] = a;
         *(tmp as *mut _) = $operation(a, b);
     };
 }
@@ -16,15 +16,32 @@ macro_rules! amo {
 /// is_atomic_instruction
 /// 
 /// Take the instruction and returns whether the instruction at that address is an atomic one
-pub unsafe fn is_atomic_instruction(insn: usize) -> bool {
+pub unsafe fn is_atomic_instruction(insn: u32) -> bool {
     (insn & 0b1111111) == 0b0101111
 }
 
 /// atomic_emulation
 /// 
 /// Takes the exception program counter and an array of registers at point of exception with [`PLATFORM_REGISTER_LEN`] length.
-pub unsafe fn atomic_emulation(insn: usize, frame: &mut [usize; PLATFORM_REGISTER_LEN]) -> bool {
+pub unsafe fn atomic_emulation(pc: usize, frame: &mut [usize; PLATFORM_REGISTER_LEN]) -> bool {
     static mut S_LR_ADDR: usize = 0;
+    let insn = if pc % 4 != 0 {
+        let prev_aligned = pc & !0x3;
+        let offset = (pc - prev_aligned) as usize; 
+
+        let buffer = (*((prev_aligned + 4) as *const u32) as u64) << 32
+            | (*(prev_aligned as *const u32) as u64);
+        let buffer_bytes = buffer.to_le_bytes();
+
+        u32::from_le_bytes([
+            buffer_bytes[offset],
+            buffer_bytes[offset + 1],
+            buffer_bytes[offset + 2],
+            0,
+        ])
+    } else {
+        *(pc as *const u32)
+    };
 
     if !is_atomic_instruction(insn) {
         return false;
@@ -41,18 +58,18 @@ pub unsafe fn atomic_emulation(insn: usize, frame: &mut [usize; PLATFORM_REGISTE
     match insn >> 27 {
         0b00010 => {
             /* LR */
-            S_LR_ADDR = frame[rs1];
+            S_LR_ADDR = frame[rs1 as usize];
             let tmp: usize = *(S_LR_ADDR as *const _);
-            frame[rd] = tmp;
+            frame[rd as usize] = tmp;
         }
         0b00011 => {
             /* SC */
-            let tmp: usize = frame[rs1];
+            let tmp: usize = frame[rs1 as usize];
             if tmp != S_LR_ADDR {
-                frame[rd] = 1;
+                frame[rd as usize] = 1;
             } else {
-                *(S_LR_ADDR as *mut _) = frame[rs2];
-                frame[rd] = 0;
+                *(S_LR_ADDR as *mut _) = frame[rs2 as usize];
+                frame[rd as usize] = 0;
                 S_LR_ADDR = 0;
             }
         }
